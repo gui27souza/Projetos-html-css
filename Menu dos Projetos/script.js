ab47a44942d352ed070aaf2d3b50651e70b420a3
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-04-17 17:36:10 -0300

    Update script.js

--- Menu dos Projetos/script.js
+++ Menu dos Projetos/script.js
@@ -1,19 +1,19 @@
 // Sistema de troca de cores automática
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    atualizarCorBase(); 
-    setInterval(atualizarCorBase, 100000); 
-});
+    document.addEventListener('DOMContentLoaded', (event) => {
+        atualizarCorBase(); 
+        setInterval(atualizarCorBase, 100000); 
+    });
 
-function atualizarCorBase() {
-    const dataAtual = new Date();
-    const horas = dataAtual.getHours();
-    const minutos = dataAtual.getMinutes();
-    const totalMinutos = horas * 60 + minutos;
-    const hue = Math.round((totalMinutos / 1440) * 360) + 120;
+    function atualizarCorBase() {
+        const dataAtual = new Date();
+        const horas = dataAtual.getHours();
+        const minutos = dataAtual.getMinutes();
+        const totalMinutos = horas * 60 + minutos;
+        const hue = Math.round((totalMinutos / 1440) * 360) + 120;
 
-    document.documentElement.style.setProperty('--cor-hue', hue.toString());
-}
+        document.documentElement.style.setProperty('--cor-hue', hue.toString());
+    }
 
 
 
@@ -27,35 +27,35 @@ function atualizarCorBase() {
     let button_text = document.getElementsByClassName('project-type__button__text')
 
     // Função que reseta a seleção
-function showTodos() {
-    for (let container of document.getElementsByClassName('container')) {
-        container.style.display = '';
+    function showTodos() {
+        for (let container of document.getElementsByClassName('container')) {
+            container.style.display = '';
+        }
+        button_text[0].style.textDecorationColor = 'var(--cor-principal)';
+        button_text[1].style.textDecorationColor = '#f0f8ff'
+        button_text[2].style.textDecorationColor = '#f0f8ff'
     }
-    button_text[0].style.textDecorationColor = 'var(--cor-principal)';
-    button_text[1].style.textDecorationColor = '#f0f8ff'
-    button_text[2].style.textDecorationColor = '#f0f8ff'
-}
 
     // Função que mostra apenas o tipo de item selecionado
-function show(elemento, botao) {
-    for (let container of document.getElementsByClassName('container')) {
-        container.style.display = 'none';
+    function show(elemento, botao) {
+        for (let container of document.getElementsByClassName('container')) {
+            container.style.display = 'none';
+        }
+        elemento.style.display = ''
+
+        button_text[botao.getAttribute('data-value')].style.textDecoration = 'underline var(--cor-principal)'
+
+        switch (botao.getAttribute('data-value')) {
+            case '1' : 
+                button_text[1].style.textDecorationColor = 'var(--cor-principal)';
+                button_text[2].style.textDecorationColor = '#f0f8ff'
+                button_text[0].style.textDecorationColor = '#f0f8ff'
+            break
+
+            case '2' : 
+                button_text[2].style.textDecorationColor = 'var(--cor-principal)';
+                button_text[1].style.textDecorationColor = '#f0f8ff'
+                button_text[0].style.textDecorationColor = '#f0f8ff'
+            break
+        }
     }
-    elemento.style.display = ''
-
-    button_text[botao.getAttribute('data-value')].style.textDecoration = 'underline var(--cor-principal)'
-
-    switch (botao.getAttribute('data-value')) {
-        case '1' : 
-            button_text[1].style.textDecorationColor = 'var(--cor-principal)';
-            button_text[2].style.textDecorationColor = '#f0f8ff'
-            button_text[0].style.textDecorationColor = '#f0f8ff'
-        break
-
-        case '2' : 
-            button_text[2].style.textDecorationColor = 'var(--cor-principal)';
-            button_text[1].style.textDecorationColor = '#f0f8ff'
-            button_text[0].style.textDecorationColor = '#f0f8ff'
-        break
-    }
-}
