8feba7ab3f26cd875015196bbf8d70759909dc3f
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-05-20 11:03:56 -0300

    Update Color System

---
+++ js/color_system.js
@@ -0,0 +1,15 @@
+// Sistema de troca de cores automática
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+        atualizarCorBase(); 
+        setInterval(atualizarCorBase, 100000); 
+    });
+
+    function atualizarCorBase() {
+        const dataAtual = new Date();
+        const hue = Math.round((((dataAtual.getHours()) * 60 + (dataAtual.getMinutes())) / 1440) * 360) + 120;
+
+        document.documentElement.style.setProperty('--cor-hue', hue.toString());
+    }
+
+//
