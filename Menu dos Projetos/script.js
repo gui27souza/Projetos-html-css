fbe3611f95bc0a12214d3b32cf81adbc82c851bd
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-05-20 09:17:53 -0300

    Update Color System

--- Menu dos Projetos/script.js
+++ Menu dos Projetos/script.js
@@ -7,7 +7,7 @@
 
     function atualizarCorBase() {
         const dataAtual = new Date();
-        const hue = Math.round((((dataAtual.getHours()) * 60 + (dataAtual.getMinutes())) / 1440) * 360);
+        const hue = Math.round((((dataAtual.getHours()) * 60 + (dataAtual.getMinutes())) / 1440) * 360) + 120;
 
         document.documentElement.style.setProperty('--cor-hue', hue.toString());
     }
@@ -18,46 +18,46 @@
 
 
 
-// Sistema de seleção de item por tipo
+// // Sistema de seleção de item por tipo
 
-    // Declaração de Variáveis
-    let button_text = document.getElementsByClassName('project-type__button__text')
+//     // Declaração de Variáveis
+//     let button_text = document.getElementsByClassName('project-type__button__text')
 
-    // Função que reseta a seleção
-    function showTodos() {
-        for (let item of document.getElementsByClassName('item')) {
-            item.style.display = '';
-        }
-        for (let hidden of document.getElementsByClassName('hidden-todos')) {
-            hidden.style.display = 'none'
-        }
+//     // Função que reseta a seleção
+//     function showTodos() {
+//         for (let item of document.getElementsByClassName('item')) {
+//             item.style.display = '';
+//         }
+//         for (let hidden of document.getElementsByClassName('hidden-todos')) {
+//             hidden.style.display = 'none'
+//         }
 
-        document.getElementById('container').style.flexFlow = 'column'
+//         document.getElementById('container').style.flexFlow = 'column'
 
-        // Reset da underline dos botões
-        for (let button of button_text) {
-            button.style.textDecorationColor = '#f0f8ff'
-        }
-        button_text[0].style.textDecorationColor = 'var(--cor-principal)'
-    }
+//         // Reset da underline dos botões
+//         for (let button of button_text) {
+//             button.style.textDecorationColor = '#f0f8ff'
+//         }
+//         button_text[0].style.textDecorationColor = 'var(--cor-principal)'
+//     }
 
-    // Função que mostra apenas o tipo de item selecionado
-    function show(elemento, botao) {
-        for (let item of document.getElementsByClassName('item')) {
-            item.style.display = 'none';
-        }
+//     // Função que mostra apenas o tipo de item selecionado
+//     function show(elemento, botao) {
+//         for (let item of document.getElementsByClassName('item')) {
+//             item.style.display = 'none';
+//         }
 
-        for (let e of elemento) {
-            e.style.display = ''
-        }
+//         for (let e of elemento) {
+//             e.style.display = ''
+//         }
 
-        document.getElementById('container').style.flexFlow = 'column-reverse'
+//         document.getElementById('container').style.flexFlow = 'column-reverse'
 
-        button_text[botao.getAttribute('data-value')].style.textDecoration = 'underline var(--cor-principal)'
+//         button_text[botao.getAttribute('data-value')].style.textDecoration = 'underline var(--cor-principal)'
 
-        for (let button of button_text) {
-            button.style.textDecorationColor = '#f0f8ff'
-        }
-        button_text[Number(botao.getAttribute('data-value'))].style.textDecorationColor = 'var(--cor-principal)'
-    }
-//
+//         for (let button of button_text) {
+//             button.style.textDecorationColor = '#f0f8ff'
+//         }
+//         button_text[Number(botao.getAttribute('data-value'))].style.textDecorationColor = 'var(--cor-principal)'
+//     }
+// //
