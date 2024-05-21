3f0336a743b6656abe1da6a97bddc0bf48e8acf3
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-05-21 09:15:58 -0300

    Update Color System

--- js/color_system.js
+++ js/color_system.js
@@ -1,15 +1,29 @@
 // Sistema de troca de cores automática
 
+    // Atualiza a cor com base em um intervalo
     document.addEventListener('DOMContentLoaded', (event) => {
         atualizarCorBase(); 
         setInterval(atualizarCorBase, 100000); 
     });
 
+    // Calcula a nova hue de acordo com dia e horário
     function atualizarCorBase() {
         const dataAtual = new Date();
-        const hue = Math.round((((dataAtual.getHours()) * 60 + (dataAtual.getMinutes())) / 1440) * 360) + 0;
+        const hue = Math.round((((dataAtual.getHours()) * 60 + (dataAtual.getMinutes())) / 1440) * 360) + dayRandom(dataAtual);
 
         document.documentElement.style.setProperty('--cor-hue', hue.toString());
+    }
+
+    // Randomiza a variável de cálculo da hue
+    function dayRandom(dataAtual) {
+        let diaAtual = dataAtual.getDate()
+        
+        if (diaAtual % 2 != 0) {
+            diaAtual += 120
+            return diaAtual
+        }
+
+        return diaAtual * 12
     }
 
 //
