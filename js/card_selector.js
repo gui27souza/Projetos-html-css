b4dff3a79eefd85ece01277030438519f9c0115c
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-05-21 10:56:28 -0300

    Update Card Selector
    Agora está completamente funcional em telas maiores

--- js/card_selector.js
+++ js/card_selector.js
@@ -2,6 +2,7 @@
 
     var all_items = document.getElementsByClassName("item-wrapper")
     var all_inputs = document.getElementsByClassName("actions__button")
+    var sobre_element = document.getElementById("sobre")
 
 // 
 
@@ -13,6 +14,10 @@
         var card_type = this_input.value
 
         if (card_type == "Todos") {
+
+            // Reseta a ordem dos cards, colocando os mais antigos primeiro
+            ajustaOrdem()
+            ordem_inversa = false
 
             // Todos visíveis
             for (let item of all_items) {
@@ -28,6 +33,9 @@
             document.getElementById("button-todos").style.textDecorationColor = "var(--cor-principal)"
 
         } else {
+
+            // Função para colocar os cards mais recentes primeiro
+            inverterOrdem()
         
             // Laço que verifica o tipo do card e muda a sua visibilidade
             for (let item of all_items) {
@@ -49,7 +57,7 @@
         }
 
         // Torna o sobre invisível, se necessário
-        document.getElementById("sobre").style.display = "none"
+        sobre_element.style.display = "none"
     }
 
 // 
@@ -70,11 +78,64 @@
         }
 
         // Torna o sobre visível
-        document.getElementById("sobre").style.display = "block"
+        sobre_element.style.display = "block"
         
         // Coloca o underline no input Sobre
         document.getElementById("button-sobre").style.textDecorationColor = "var(--cor-principal)"
 
+    }
+
+// 
+
+
+
+
+// Variaveis
+
+    // Página se inicializa com ordem normal
+    var ordem_inversa = false
+
+// 
+
+// Função apra inverter a ordem caso esteja na ordem padrão
+
+    function inverterOrdem() {
+
+        // Checa se a ordem já não está inversa
+        if (!ordem_inversa) {
+
+            // Converte HTMLCollection para um array
+            var itemsArray = Array.from(all_items)
+
+            // Inverte a ordem do array
+            itemsArray.reverse()
+
+            // Pega o elemento pai
+            var parent = itemsArray[0].parentElement
+
+            // Adiciona os itens de volta na ordem invertida
+            itemsArray.forEach(item => {
+                parent.appendChild(item)
+            })
+
+            // Atualiza a variável de controle de ordem
+            ordem_inversa = true
+        }
+    }
+
+// 
+
+// Função para resetar a ordem
+
+    function ajustaOrdem() {
+
+        // Só reseta se já estiver na ordem inversa
+        if (ordem_inversa) {
+            ordem_inversa = false
+            inverterOrdem()
+            ordem_inversa = false
+        } 
+        
     }
 
 // 
